d5cb0be9fa4a46421b5d7ebd67edbf978a8a3592
selametsamli <selamet96@gmail.com>
2020-05-04 23:36:19 +0300

    Created histories models

--- personal/admin.py
+++ personal/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from personal.models import PersonalDetail
+from personal.models import PersonalDetail, Profile, Education, Ability, Language, Course, Experience, Hobby, Reference, \
+    Achievement, Publication
 
 # Register your models here.
 
 
 admin.site.register(PersonalDetail)
+admin.site.register(Profile)
+admin.site.register(Education)
+admin.site.register(Ability)
+admin.site.register(Language)
+admin.site.register(Course)
+admin.site.register(Experience)
+admin.site.register(Hobby)
+admin.site.register(Reference)
+admin.site.register(Achievement)
+admin.site.register(Publication)
--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ from django.utils import timezone
 
 
 class PersonalDetail(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(null=True, blank=True, max_length=50)
     surname = models.CharField(null=True, blank=True, max_length=50)
     email = models.EmailField(verbose_name='email')
@@ -25,3 +27,202 @@ class PersonalDetail(models.Model):
             self.created = timezone.now()
         self.modified = timezone.now()
         return super(PersonalDetail, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, verbose_name='user', null=True)
+    content = models.TextField(blank=True)
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Profile, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Education(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+    agree = models.CharField(verbose_name='Derece', max_length=100, null=False, blank=False)
+    city = models.CharField(verbose_name='City', max_length=55)
+    school = models.CharField(verbose_name='School', max_length=255)
+    starter_date = models.DateField()
+    end_date = models.DateField()
+    content = models.TextField(verbose_name='Content')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Education, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.agree
+
+
+class Experience(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    jobTitle = models.CharField(verbose_name='JobTitle', max_length=100, null=False, blank=False)
+    city = models.CharField(verbose_name='City', max_length=55)
+    employer = models.CharField(verbose_name='Employer', max_length=255)
+    starter_date = models.DateField()
+    end_date = models.DateField()
+    content = models.TextField(verbose_name='Content')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Experience, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.jobTitle
+
+
+class Ability(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    name = models.CharField(max_length=100, verbose_name='Name')
+    level = models.IntegerField(verbose_name='level')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Ability, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.name
+
+
+class Language(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+
+    language = models.CharField(max_length=100, verbose_name='Name')
+    level = models.IntegerField(verbose_name='level')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Language, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.language
+
+
+class Reference(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+
+    company = models.CharField(max_length=100, verbose_name='company')
+    person = models.CharField(max_length=100, verbose_name='person')
+    telephone = models.CharField(max_length=55, verbose_name='Telephone')
+    email = models.EmailField()
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Reference, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.company
+
+
+class Hobby(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+
+    name = models.CharField(max_length=100, verbose_name='Name')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Hobby, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.name
+
+
+class Course(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    name = models.CharField(verbose_name='Derece', max_length=100, null=False, blank=False)
+    institution = models.CharField(verbose_name='City', max_length=255)
+    starter_date = models.DateField()
+    end_date = models.DateField()
+    content = models.TextField(verbose_name='Content')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Course, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.name
+
+
+class Achievement(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    content = models.TextField(verbose_name='Content')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Achievement, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.id
+
+
+class Publication(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    content = models.TextField(verbose_name='Content')
+
+    created = models.DateTimeField(editable=False, null=True, blank=True)
+    modified = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Publication, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.id
